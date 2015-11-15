a4f646e7dc566e833b38196855b3cf56874c2b8d
Helen See <helen.see@rutgers.edu>
2015-11-15 01:23:18 -0500

    connected to db
    general, doesn't grab anything important

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 var express = require('express');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'foodsnap.c7tcssw8uobt.us-east-1.rds.amazonaws.com',
+	port: '3306',
+	user: 'foodsnap',
+	password: 'foodsnap',
+	database: 'FoodSNAP'
+});
 var app = express();
+
+connection.connect(function(err){
+	if(!err){
+		console.log("Database if connected :D \n\n");
+	}else{
+		console.log("Error connecting database D: \n\n");
+	}
+	
+});
+
+connection.query('SELECT 1+1 AS solution FROM company_users', function(err, rows, fields){
+if(err) throw err;
+console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
 
 app.set('port', (process.env.PORT || 5000));
 
